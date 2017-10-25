2943b0a8b86d525efd361003b8ce7ed69d5496e3
Mikhail <mikhail0234@gmail.com>
2017-10-25 19:13:39 +0300

    Homework1

---
+++ CoinMap/CoinMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinMap
+//
+//  Created by Mikhail on 25.10.17.
+//  Copyright © 2017 Mikhail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
